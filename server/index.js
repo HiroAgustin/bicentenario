0496094a8488a7d292b2c88b6f413a78b9954069
HiroAgustin <hiroagustin@gmail.com>
2013-12-11 00:08:30 -0200

    add request error handling

--- server/index.js
+++ server/index.js
@@ -24,36 +24,36 @@ app.use(app.router);
 // Since this is the last non-error-handling
 // middleware use()d, we assume 404, as nothing else
 // responded.
-// app.use(function (req, res, next)
-// {
-//     res.status(404);
+app.use(function (req, res, next)
+{
+    res.status(404);
   
-//     // respond with html page
-//     if (req.accepts('html'))
-//     {
-//         res.render('404', { url: req.url });
-//         return;
-//     }
+    // respond with html page
+    if (req.accepts('html'))
+    {
+        res.render('404', { url: req.url });
+        return;
+    }
 
-//     // respond with json
-//     if (req.accepts('json'))
-//     {
-//         res.send({ error: 'Not found' });
-//         return;
-//     }
+    // respond with json
+    if (req.accepts('json'))
+    {
+        res.send({ error: 'Not found' });
+        return;
+    }
 
-//     // default to plain-text. send()
-//     res.type('txt').send('Not found');
-// });
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
 
-// app.use(function (err, req, res, next)
-// {
-//   // we may use properties of the error object
-//   // here and next(err) appropriately, or if
-//   // we possibly recovered from the error, simply next().
-//   res.status(err.status || 500);
-//   res.render('500', { error: err });
-// });
+app.use(function (err, req, res, next)
+{
+  // we may use properties of the error object
+  // here and next(err) appropriately, or if
+  // we possibly recovered from the error, simply next().
+  res.status(err.status || 500);
+  res.render('500', { error: err });
+});
 
 app.get('/', function (req, res)
 {
@@ -62,29 +62,29 @@ app.get('/', function (req, res)
     });
 });
 
-// app.get('/404', function (req, res, next)
-// {
-//   // trigger a 404 since no other middleware
-//   // will match /404 after this one, and we're not
-//   // responding here
-//   next();
-// });
+app.get('/404', function (req, res, next)
+{
+  // trigger a 404 since no other middleware
+  // will match /404 after this one, and we're not
+  // responding here
+  next();
+});
 
-// app.get('/403', function (req, res, next)
-// {
-//   // trigger a 403 error
-//   var err = new Error('not allowed!');
-//   err.status = 403;
-//   next(err);
-// });
+app.get('/403', function (req, res, next)
+{
+  // trigger a 403 error
+  var err = new Error('not allowed!');
+  err.status = 403;
+  next(err);
+});
 
-// app.get('/500', function (req, res, next)
-// {
-//   // trigger a generic (500) error
-//   next(new Error('keyboard cat!'));
-// });
+app.get('/500', function (req, res, next)
+{
+  // trigger a generic (500) error
+  next(new Error('keyboard cat!'));
+});
 
 if (!module.parent)
 {
-    app.listen(process.env.PORT || 8080);
+    app.listen(process.env.PORT || 9000);
 }
