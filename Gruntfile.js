4192aaa82f10dc45da7357bda0ff4234d96e6a8a
HiroAgustin <hiroagustin@gmail.com>
2013-12-15 23:19:13 -0200

    homepage

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function (grunt)
         }
     ,   watch: {
             compass: {
-                files: ['public/scss/*.scss']
+                files: ['public/scss/*.scss', 'server/**/*.ejs']
             ,   tasks: ['compass']
             ,   options: {
                     livereload: true
