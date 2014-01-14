36b6f7745e73b8484416721b780ec9266773b332
HiroAgustin <hiroagustin@gmail.com>
2014-01-14 19:03:12 -0200

    mocking api response

--- server/controllers/law/index.js
+++ server/controllers/law/index.js
@@ -1,9 +1,37 @@
 var controller = require(__dirname + '/../../lib/controller.js')
 ,   app = module.exports = controller(__dirname);
 
-app.get('/user', function (req, res)
+app.get('/mis-leyes', function (req, res)
 {
+    var api = {
+        nombre: 'Maria Jose'
+    ,   leyes: [
+            {
+                title: 'Salud Sexual y Reproductiva'
+            ,   icon: ''
+            ,   description: 'A ponerla'
+            ,   url: '/aaaaapo'
+            ,   year: 2010
+            }
+        ,   {
+                title: 'Maternidad'
+            ,   icon: ''
+            ,   description: 'Tas gorda mija'
+            ,   url: '/pregos'
+            ,   year: 2010
+            }
+        ,   {
+                title: 'Empleo'
+            ,   icon: ''
+            ,   description: 'Si o si laburas'
+            ,   url: '/moneymoney'
+            ,   year: 2013
+            }
+        ]
+    ,   comparten: 50000
+    };
+
     res.render('user', {
-        title: 'User'
+        title: 'Leyes de ' + api.nombre
     });
 });
