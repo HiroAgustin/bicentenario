a218b92a546f4db438d6c5d58099de2ca670eb72
HiroAgustin <hiroagustin@gmail.com>
2014-06-17 22:29:05 -0300

    pass user data to form

--- server/controllers/landing/index.js
+++ server/controllers/landing/index.js
@@ -3,13 +3,18 @@ var controller = require(__dirname + '/../../lib/controller.js')
 
 ,   getAttr = function getAttribute (user, attribute)
     {
-        return user && user[attribute] || '';
+        return user && user._json && user._json[attribute] || '';
     }
 
 ,   parseUser = function parseUser (user)
     {
+        var gender = getAttr(user, 'gender')
+        ,   birthday = getAttr(user, 'birthday');
+
         return {
-            name: getAttr(user, 'displayName')
+            name: getAttr(user, 'first_name')
+        ,   isMale: gender === 'male'
+        ,   isFemale: gender === 'female'
         }
     };
 
