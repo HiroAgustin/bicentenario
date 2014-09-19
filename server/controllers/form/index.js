7acfb030ad18b9dac89b9a08e137c1af8ca80bb7
HiroAgustin <hiroagustin@gmail.com>
2014-09-18 23:14:02 -0300

    dynamicly append body parts

--- server/controllers/form/index.js
+++ server/controllers/form/index.js
@@ -1,40 +1,49 @@
 var controller = require(__dirname + '/../../lib/controller.js')
 
-,	app = module.exports = controller(__dirname)
+	,	charactersPath = __dirname + '/../../characters/'
 
-,	getAge = function getAge (dateString)
-	{
-		var today = new Date()
-			,	birthDate = new Date(dateString)
-			,	years = today.getFullYear() - birthDate.getFullYear()
-			,	month = today.getMonth() - birthDate.getMonth();
+	,	app = module.exports = controller(__dirname)
 
-		if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
-			years--;
+	,	fs = require('fs')
 
-		return years;
-	}
+	,	getAge = function getAge (dateString)
+		{
+			var today = new Date()
+				,	birthDate = new Date(dateString)
+				,	years = today.getFullYear() - birthDate.getFullYear()
+				,	month = today.getMonth() - birthDate.getMonth();
 
-,	getAttr = function getAttribute (user, attribute)
-	{
-		return user && user._json && user._json[attribute] || '';
-	}
+			if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
+				years--;
 
-,	parseUser = function parseUser (user)
-	{
-		return {
-			name: getAttr(user, 'first_name')
-		,	gender: getAttr(user, 'gender')
-		,	age: getAge(getAttr(user, 'birthday'))
-		,	status: getAttr(user, 'relationship_status')
-		,	location: getAttr(user, 'location').name
+			return years;
 		}
-	};
+
+	,	getAttr = function getAttribute (user, attribute)
+		{
+			return user && user._json && user._json[attribute] || '';
+		}
+
+	,	parseUser = function parseUser (user)
+		{
+			return {
+				name: getAttr(user, 'first_name')
+			,	gender: getAttr(user, 'gender')
+			,	age: getAge(getAttr(user, 'birthday'))
+			,	status: getAttr(user, 'relationship_status')
+			,	location: getAttr(user, 'location').name
+			}
+		};
 
 app.get('/ingresar', function (req, res)
 {
 	res.render('form', {
 		title: 'Ingresar mi informacón'
 	,	user: parseUser(req.user)
+	,	bodyParts: {
+			heads: fs.readdirSync(charactersPath + 'heads/').length
+		,	bodys: fs.readdirSync(charactersPath + 'bodys/').length
+		,	legs: fs.readdirSync(charactersPath + 'legs/').length
+		}
 	});
 });
