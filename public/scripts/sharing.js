05a8493ff154277225e2d62de66c8e599e9caf71
HiroAgustin <hiroagustin@gmail.com>
2014-09-23 17:57:26 -0300

    remove link-circle-sm as only size used, refactor sharing links script includes, experimenting with facebook share api - need deployment

--- public/scripts/sharing.js
+++ public/scripts/sharing.js
@@ -1,47 +1,63 @@
-(function (win, doc, s, fb, tw, ggl, js, fjs)
+(function (win, doc, s, js, fjs, inc)
 {
-    fjs = doc.getElementsByTagName(s)[0];
+  fjs = doc.getElementsByTagName(s)[0];
 
-    // https://developers.facebook.com/docs/javascript/quickstart/v2.0#loading
-    win.fbAsyncInit = function ()
-    {
-      FB.init({
-        appId: '144441392317207'
-      , xfbml: true
-      , version: 'v2.0'
-      });
-    };
-
-    // <!-- Facebook Share Button -->
+  inc = function (src)
+  {
     js = doc.createElement(s);
-    js.id = fb;
-    js.src = '//connect.facebook.net/es_LA/sdk.js';
-    fjs.parentNode.insertBefore(js, fjs);
-    // <!-- End Facebook Share Button -->
-
-    // <!-- Twitter Button -->
-    js = doc.createElement(s);
-    js.id = tw;
-    js.src = '//platform.twitter.com/widgets.js';
-    fjs.parentNode.insertBefore(js, fjs);
-    // <!-- End Twitter Button -->
-
-    // <!-- Google Analytics -->
-    win['GoogleAnalyticsObject'] = 'ga';
-
-    win.ga = function ()
-    {
-      (win.ga.q = win.ga.q || []).push(arguments);
-    };
-
-    win.ga.l = 1 * new Date();
-
+    js.src = src;
     js.async = 1;
-    js.src = ggl;
-    fjs.parentNode.insertBefore(js, fjs)
+    fjs.parentNode.insertBefore(js, fjs);
+  };
 
-    ga('create', 'UA-XXXX-Y', 'auto');
-    ga('send', 'pageview');
-    // <!-- End Google Analytics -->
+  // https://developers.facebook.com/docs/javascript/quickstart/v2.0#loading
+  win.fbAsyncInit = function ()
+  {
+    FB.init({
+      appId: '144441392317207'
+    , xfbml: true
+    , version: 'v2.0'
+    });
+  };
 
-}(window, document, 'script', 'facebook-jssdk', 'twitter-wjs', '//www.google-analytics.com/analytics.js'))
+  _.on('[data-share="facebook"]', 'click', function (event)
+  {
+    event.preventDefault();
+
+    FB.ui(
+      {
+        method: 'share'
+      , href: this.getAttribute('href')
+      }
+    , function (response)
+      {
+
+      }
+    );
+  });
+
+  // <!-- Facebook Share Button -->
+  inc('//connect.facebook.net/es_LA/sdk.js');
+  // <!-- End Facebook Share Button -->
+
+  // <!-- Twitter Button -->
+  inc('//platform.twitter.com/widgets.js');
+  // <!-- End Twitter Button -->
+
+  // <!-- Google Analytics -->
+  inc('//www.google-analytics.com/analytics.js');
+
+  win['GoogleAnalyticsObject'] = 'ga';
+
+  win.ga = function ()
+  {
+    (win.ga.q = win.ga.q || []).push(arguments);
+  };
+
+  win.ga.l = 1 * new Date();
+
+  ga('create', 'UA-XXXX-Y', 'auto');
+  ga('send', 'pageview');
+  // <!-- End Google Analytics -->
+
+}(window, document, 'script'))
