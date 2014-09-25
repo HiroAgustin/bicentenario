e0abc7ae8e352a9376e794c5ce49e9f08ea8accd
HiroAgustin <hiroagustin@gmail.com>
2014-09-25 15:59:38 -0300

    api returns article first

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -56,8 +56,7 @@ var controller = require(__dirname + '/../../lib/controller.js')
 			,	title: law.nombre
 			,	category: law.categoria
 			,	priority: law.prioridad
-			// , articles: [law.art1[0]]
-			,	articles: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
+			, articles: [law.art1[0]]
 			,	year: date.getFullYear()
 			,	fecha: date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
 			};
