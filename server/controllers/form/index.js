6d040633bbca6ed172fd36c0fe52e1d1b77604ad
HiroAgustin <hiroagustin@gmail.com>
2014-10-09 14:51:28 -0400

    jshinted

--- server/controllers/form/index.js
+++ server/controllers/form/index.js
@@ -1,52 +1,54 @@
-var controller = require(__dirname + '/../../lib/controller.js')
+;(function (controller, fs)
+{
+	'use strict';
 
-	,	charactersPath = __dirname + '/../../characters/'
+	var charactersPath = __dirname + '/../../characters/'
 
-	,	app = module.exports = controller(__dirname)
+		,	app = module.exports = controller(__dirname)
 
-	,	fs = require('fs')
+		,	getAge = function getAge (dateString)
+			{
+				var today = new Date()
+					,	birthDate = new Date(dateString)
+					,	years = today.getFullYear() - birthDate.getFullYear()
+					,	month = today.getMonth() - birthDate.getMonth();
 
-	,	getAge = function getAge (dateString)
-		{
-			var today = new Date()
-				,	birthDate = new Date(dateString)
-				,	years = today.getFullYear() - birthDate.getFullYear()
-				,	month = today.getMonth() - birthDate.getMonth();
+				if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
+					years--;
 
-			if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
-				years--;
-
-			return years;
-		}
-
-	,	getAttr = function getAttribute (user, attribute)
-		{
-			return user && user._json && user._json[attribute] || '';
-		}
-
-	,	parseUser = function parseUser (user)
-		{
-			return {
-				name: getAttr(user, 'first_name')
-			,	gender: getAttr(user, 'gender')
-			,	age: getAge(getAttr(user, 'birthday'))
-			,	status: getAttr(user, 'relationship_status')
-			,	location: getAttr(user, 'location').name
+				return years;
 			}
-		}
 
-	,	bodyParts = {};
+		,	getAttr = function getAttribute (user, attribute)
+			{
+				return user && user._json && user._json[attribute] || '';
+			}
 
-fs.readdirSync(charactersPath).forEach(function (item)
-{
-	bodyParts[item] = fs.readdirSync(charactersPath + item);
-});
+		,	parseUser = function parseUser (user)
+			{
+				return {
+					name: getAttr(user, 'first_name')
+				,	gender: getAttr(user, 'gender')
+				,	age: getAge(getAttr(user, 'birthday'))
+				,	status: getAttr(user, 'relationship_status')
+				,	location: getAttr(user, 'location').name
+				};
+			}
 
-app.get('/ingresar', function (req, res)
-{
-	res.render('form', {
-		title: 'Ingresar mi informacón'
-	,	user: parseUser(req.user)
-	,	bodyParts: bodyParts
+		,	bodyParts = {};
+
+	fs.readdirSync(charactersPath).forEach(function (item)
+	{
+		bodyParts[item] = fs.readdirSync(charactersPath + item);
 	});
-});
+
+	app.get('/ingresar', function (req, res)
+	{
+		res.render('form', {
+			title: 'Ingresar mi informacón'
+		,	user: parseUser(req.user)
+		,	bodyParts: bodyParts
+		});
+	});
+
+}(require(__dirname + '/../../lib/controller.js'), require('fs')));
