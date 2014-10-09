e8cedff1d5e6db91f23ed87a2b0045a1e564fc36
HiroAgustin <hiroagustin@gmail.com>
2014-10-09 16:40:31 -0400

    jshinted

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
-var express = require('express')
-	app = module.exports = express();
+;(function (express)
+{
+	'use strict';
 
-app.use(express.static(__dirname + '/../public'), {
-	maxAge: 604800000
-});
+	var app = module.exports = express();
 
-app.enable('verbose errors');
+	app.use(express.static(__dirname + '/../public'), {
+		maxAge: 604800000
+	});
 
-app.use(require('./controllers/landing'));
-app.use(require('./controllers/facebook'));
+	app.enable('verbose errors');
 
-app.use(require('./controllers/form'));
-app.use(require('./controllers/user'));
-app.use(require('./controllers/laws'));
+	app.use(require('./controllers/landing'));
+	app.use(require('./controllers/facebook'));
 
-app.use(require('./controllers/error'));
+	app.use(require('./controllers/form'));
+	app.use(require('./controllers/user'));
+	app.use(require('./controllers/laws'));
 
-app.listen(process.env.PORT || 9000);
+	app.use(require('./controllers/error'));
+
+	app.listen(process.env.PORT || 9000);
+
+}(require('express')));
