711a749296c1e72cab3811bfcf69c0a55a735804
HiroAgustin <hiroagustin@gmail.com>
2014-10-15 12:04:05 -0400

    developer playground

--- server/controllers/landing/index.js
+++ server/controllers/landing/index.js
@@ -1,25 +1,35 @@
-var controller = require(__dirname + '/../../lib/controller.js')
-,	app = module.exports = controller(__dirname)
-
-app.get('/', function (req, res)
+;(function (controller)
 {
-	res.render('home', {
-		path: req.path
-	});
-});
+	'use strict';
 
-app.get('/linea-de-tiempo', function (req, res)
-{
-	res.render('timeline', {
-		title: 'Linea de tiempo'
-	,	path: req.path
-	});
-});
+	var app = module.exports = controller(__dirname);
 
-app.get('/que-es-este-proyecto', function (req, res)
-{
-	res.render('info', {
-		title: '¿Qué es este proyecto?'
-	,	path: req.path
+	app.get('/', function (req, res)
+	{
+		res.render('home', {
+			path: req.path
+		});
 	});
-});
+
+	app.get('/linea-de-tiempo', function (req, res)
+	{
+		res.render('timeline', {
+			title: 'Linea de tiempo'
+		,	path: req.path
+		});
+	});
+
+	app.get('/que-es-este-proyecto', function (req, res)
+	{
+		res.render('info', {
+			title: '¿Qué es este proyecto?'
+		,	path: req.path
+		});
+	});
+
+	app.get('/test', function (req, res)
+	{
+		res.render('test');
+	});
+
+}(require(__dirname + '/../../lib/controller.js')));
