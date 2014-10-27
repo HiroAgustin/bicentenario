f8562f50d4cb6919827f1678afe6703f2e522ef6
HiroAgustin <hiroagustin@gmail.com>
2014-10-27 15:09:40 -0400

    jshint valid

--- server/controllers/facebook/index.js
+++ server/controllers/facebook/index.js
@@ -1,60 +1,64 @@
-var passport = require('passport')
-,	FacebookStrategy = require('passport-facebook').Strategy;
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Facebook profile is serialized
-//   and deserialized.
-passport.serializeUser(function (user, done)
+;(function (passport, passportFacebook, controller)
 {
-	done(null, user);
-});
+  'use strict';
 
-passport.deserializeUser(function (obj, done)
-{
-	done(null, obj);
-});
+  var FacebookStrategy = passportFacebook.Strategy;
 
-// Use the FacebookStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Facebook
-//   profile), and invoke a callback with a user object.
-passport.use(new FacebookStrategy(
+	// Passport session setup.
+	//   To support persistent login sessions, Passport needs to be able to
+	//   serialize users into and deserialize users out of the session.  Typically,
+	//   this will be as simple as storing the user ID when serializing, and finding
+	//   the user by ID when deserializing.  However, since this example does not
+	//   have a database of user records, the complete Facebook profile is serialized
+	//   and deserialized.
+	passport.serializeUser(function (user, done)
 	{
-		clientID: 144441392317207
-	,	clientSecret: 'a82599dde1be3f8ad3986d203125e9f3'
-	,	callbackURL: '/auth/facebook/callback'
-	,	profileFields: ['first_name', 'gender', 'birthday', 'relationship_status', 'location']
-	}
-,	function (accessToken, refreshToken, profile, done)
+		done(null, user);
+	});
+
+	passport.deserializeUser(function (obj, done)
 	{
-		// asynchronous verification, for effect...
-		process.nextTick(function ()
+		done(null, obj);
+	});
+
+	// Use the FacebookStrategy within Passport.
+	//   Strategies in Passport require a `verify` function, which accept
+	//   credentials (in this case, an accessToken, refreshToken, and Facebook
+	//   profile), and invoke a callback with a user object.
+	passport.use(new FacebookStrategy(
 		{
-			// To keep the example simple, the user's Facebook profile is returned to
-			// represent the logged-in user. In a typical application, you would want
-			// to associate the Facebook account with a user record in your database,
-			// and return that user instead.
-			return done(null, profile);
-		});
-	}
-));
+			clientID: 144441392317207
+		,	clientSecret: 'a82599dde1be3f8ad3986d203125e9f3'
+		,	callbackURL: '/auth/facebook/callback'
+		,	profileFields: ['first_name', 'gender', 'birthday', 'relationship_status', 'location']
+		}
+	,	function (accessToken, refreshToken, profile, done)
+		{
+			// asynchronous verification, for effect...
+			process.nextTick(function ()
+			{
+				// To keep the example simple, the user's Facebook profile is returned to
+				// represent the logged-in user. In a typical application, you would want
+				// to associate the Facebook account with a user record in your database,
+				// and return that user instead.
+				return done(null, profile);
+			});
+		}
+	));
 
-var controller = require(__dirname + '/../../lib/controller.js')
-,	app = module.exports = controller();
+	var app = module.exports = controller();
 
-app.use(passport.initialize());
-app.use(passport.session());
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-app.get('/auth/facebook', passport.authenticate('facebook', {
-	scope: ['user_relationships', 'user_birthday', 'user_location']
-,	display: 'touch'
-}));
+	app.get('/auth/facebook', passport.authenticate('facebook', {
+		scope: ['user_relationships', 'user_birthday', 'user_location']
+	,	display: 'touch'
+	}));
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-	failureRedirect: '/'
-,	successRedirect: '/ingresar'
-}));
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+		failureRedirect: '/'
+	,	successRedirect: '/ingresar'
+	}));
+
+}(require('passport'), require('passport-facebook'), require(__dirname + '/../../lib/controller.js')));
